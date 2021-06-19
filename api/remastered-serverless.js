d41285482e1c9c4cbdc3bbfd018063d9e6e9539b
Gal Schlezinger <gal@spitfire.co.il>
2021-06-19 11:16:19 +0300

    move db

--- api/remastered-serverless.js
+++ api/remastered-serverless.js
@@ -1,7 +1,15 @@
 import { createVercelFunction } from "@remastered/vercel";
 import * as serverEntry from "../dist/server/entry.server";
 import path from "path";
+import fs from "fs-extra";
+import os from "os";
 
 const rootDir = path.join(__dirname, "..");
+
+const dbDir = path.join(os.tmpdir(), "db");
+fs.mkdirpSync(dbPath);
+fs.copySync(path.join(__dirname, "../dist/db"), dbDir);
+
+process.env.DATABASE_URL = `sqlite:${dbDir}/db.sqlite3`;
 
 export default createVercelFunction({ rootDir, serverEntry });
